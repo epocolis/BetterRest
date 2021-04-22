55df98e07741bf5083d079750847684ede418b6e
leotis <mekdemcrossit@tutanota.com>
2021-04-22 16:01:23 -0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Leotis buchanan on 2021-04-22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
